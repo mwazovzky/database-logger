4092cafa17cffcfb890253f9c4e3dfe9f26e115e
Mike Wazovzky <mike.wazovzky@gmail.com>
2019-04-08 22:34:26 +0300

    Package ServiceProvider.

---
+++ src/DatabaseLoggerServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace MWazovzky\DatabaseLogger;
+
+use Illuminate\Support\ServiceProvider;
+
+class DatabaseLoggerServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+}
